bf6e4125d881f00ee32ed74ef764b05c826f3071
Delano0 <delano0@mail.ru>
2016-05-13 11:11:47 +0300

    c sharp web UI testing

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    
+    class Program
+    {
+        static void Main() { }
+        IWebDriver driver = new ChromeDriver();
+    
+        [SetUp]
+     public void Initialz()
+     {
+         driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+     }
+
+        [Test]
+        public void ExecuteTest()
+        {   /*
+             * This is test 
+             * here u use methods for parts of web UI
+             * for example textbox,buttons,links etx
+             */
+            SeleniumMethods.EnterText(driver, "Name", "UserName", "Delano0");
+            SeleniumMethods.EnterText(driver, "Name", "Password", "78217tc");
+            SeleniumMethods.ClickElement(driver, "Name", "Login");
+            SeleniumMethods.EnterText(driver, "Id", "Initial", "Dell");
+            SeleniumMethods.SelectDDL(driver, "TitleId", "Id", "Mr");
+            SeleniumMethods.EnterText(driver,"Id","FirstName","Delanop0");
+            SeleniumMethods.ClickElement(driver, "Name", "Hindi");
+        }
+        [TearDown]
+        public void CleanUp()
+        {   //closer driver
+            driver.Close();
+        }
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/SeleniumMethods.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+namespace ConsoleApplication4
+{
+    class SeleniumMethods
+    {
+        //custom method for enter text 
+        public static void EnterText(IWebDriver driver, string elementType,string elementValue, string textValue)
+        {   
+             if (elementType == "Id")
+                 driver.FindElement(By.Id(elementValue)).SendKeys(textValue);
+            else if (elementType == "Name")
+                 driver.FindElement(By.Name(elementValue)).SendKeys(textValue);
+        }
+        //custom method for click
+        public static void ClickElement(IWebDriver driver,string elementType,string elementValue)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(elementValue)).Click();
+            }
+            else if (elementType == "Name")
+                driver.FindElement(By.Name(elementValue)).Click();
+        }
+        public static void SelectDDL(IWebDriver driver,string element,string elementType,string elementValue)
+        {
+
+            if (elementType == "Id")
+            {
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(elementValue);
+            }
+            else if (elementType == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(elementValue);
+        }
+
+    }
+}
